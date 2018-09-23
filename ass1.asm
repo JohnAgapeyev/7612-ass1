65fe09d8fcd7334e9665b6800bd8d94549397267
John Agapeyev <jagapeyev@gmail.com>
2018-09-23 11:21:56 -0700

    Got the bare minimum IO going

--- ass1.asm
+++ ass1.asm
@@ -1,47 +1,109 @@
 section .data
     IndexPrompt db 'Enter the index value: '
+    indl equ $-IndexPrompt
     Const1 db 'Enter the first constant value: '
+    const1l equ $-Const1
     Const2 db 'Enter the second constant value: '
+    const2l equ $-Const2
     Const3 db 'Enter the third constant value: '
+    const3l equ $-Const3
 
 section .bss
-    index resb 5
-    val1 resb 5
-    val2 resb 5
-    val3 resb 5
+    index resb 4
+    val1 resb 4
+    val2 resb 4
+    val3 resb 4
 
 section .text
 global _start
 
 _start:
+    ;Write
     mov eax, 4
     mov ebx, 1
-    mov ecx, 34
-    mov edx, 5
+    mov ecx, IndexPrompt
+    mov edx, indl
     int 0x80
-    ;call write_value
 
+    ;Read
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, index
+    mov edx, 4
+    int 0x80
+
+    ;Write
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, Const1
+    mov edx, const1l
+    int 0x80
+
+    ;Read
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, val1
+    mov edx, 4
+    int 0x80
+
+    ;Write
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, Const2
+    mov edx, const2l
+    int 0x80
+
+    ;Read
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, val2
+    mov edx, 4
+    int 0x80
+
+    ;Write
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, Const3
+    mov edx, const3l
+    int 0x80
+
+    ;Read
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, val3
+    mov edx, 4
+    int 0x80
+
+    ;Write
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, index
+    mov edx, 4
+    int 0x80
+
+    ;Write
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, val1
+    mov edx, 4
+    int 0x80
+
+    ;Write
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, val2
+    mov edx, 4
+    int 0x80
+
+    ;Write
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, val3
+    mov edx, 4
+    int 0x80
+
+    ;Exit
     mov eax, 1
     mov ebx, 0
     int 0x80
 
-;eax - address
-read_value:
-    ;Read user input
-    mov ecx, eax
-    mov eax, 3
-    mov ebx, 0
-    mov edx, 4
-    int 0x80
-    ret
-
-;eax - address
-;ebx - len to write
-write_value:
-    ;Write data to stdout
-    mov ecx, eax
-    mov edx, ebx
-    mov eax, 4
-    mov ebx, 1
-    int 0x80
-    ret
